3638e59fbe978fbb2d6436f9d822bdd93465ac19
zbz <nnzbz@163.com>
2018-05-08 15:24:50 +0800

    * 修改了注册用户消息的exchange和queue的名称

--- suc-api/src/main/java/rebue/suc/co/SucExchangeCo.java
+++ suc-api/src/main/java/rebue/suc/co/SucExchangeCo.java
@@ -1,5 +1,6 @@
 package rebue.suc.co;
 
 public class SucExchangeCo {
-    public final static String SUC_USER_ADD_EXCHANGE_NAME = "rebue.suc.pub.user.add";
+    public final static String SUC_USER_ADD_EXCHANGE_NAME = "rebue.suc.user.add.exchange";
+    public final static String SUC_USER_ADD_QUEUE_NAME    = "rebue.suc.user.add.queue";
 }
