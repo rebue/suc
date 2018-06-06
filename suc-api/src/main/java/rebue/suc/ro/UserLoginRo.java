6d2bf8eec2e41674e739a938aa4a495ba96c4ebe
zbz <nnzbz@163.com>
2018-06-06 11:07:03 +0800

    * 用户登录和注册成功返回的过期时间格式化

--- suc-api/src/main/java/rebue/suc/ro/UserLoginRo.java
+++ suc-api/src/main/java/rebue/suc/ro/UserLoginRo.java
@@ -2,6 +2,7 @@ package rebue.suc.ro;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -28,6 +29,7 @@ public class UserLoginRo {
     @ApiModelProperty(value = "签名(成功后可将签名放入Cookie中)")
     private String         sign;
     @ApiModelProperty(value = "签名过期时间(成功后可将签名过期时间放入Cookie中)")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date           expirationTime;
 
     public Long getUserId() {
