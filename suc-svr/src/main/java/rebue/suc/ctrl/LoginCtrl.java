6bece6d80e9191711e5693f5a9d17cb6f27bcec4
zbz <nnzbz@163.com>
2018-06-27 09:43:38 +0800

    * 不再获取客户端MAC地址

--- suc-svr/src/main/java/rebue/suc/ctrl/LoginCtrl.java
+++ suc-svr/src/main/java/rebue/suc/ctrl/LoginCtrl.java
@@ -1,10 +1,12 @@
 package rebue.suc.ctrl;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +23,19 @@ import rebue.suc.to.LoginByLoginNameTo;
 import rebue.suc.to.LoginByQqTo;
 import rebue.suc.to.LoginByUserNameTo;
 import rebue.suc.to.LoginByWxTo;
+import rebue.wheel.AgentUtils;
 import rebue.wheel.turing.JwtUtils;
 
 @Api(tags = "用户登录")
 @RestController
 public class LoginCtrl {
     private final static Logger _log = LoggerFactory.getLogger(LoginCtrl.class);
+
+    /**
+     * 前面经过的代理
+     */
+    @Value("${suc.passProxy:noproxy}")
+    private String              passProxy;
 
     @Resource
     private SucUserSvc          svc;
@@ -38,8 +47,11 @@ public class LoginCtrl {
      */
     @ApiOperation("用户通过登录名称(LoginName)登录\n(1: 成功;0: 缓存失败;-1: 参数不正确;-2: 找不到用户信息;-3: 密码错误;-4: 账号被锁定)")
     @PostMapping("/user/login/by/login/name")
-    UserLoginRo loginByLoginName(@RequestBody LoginByLoginNameTo loginTo, HttpServletResponse resp) {
+    UserLoginRo loginByLoginName(@RequestBody LoginByLoginNameTo loginTo, HttpServletRequest req, HttpServletResponse resp) {
         _log.info("login: " + loginTo);
+        loginTo.setIp(AgentUtils.getIpAddr(req, passProxy));
+        loginTo.setUserAgent(AgentUtils.getUserAgent(req));
+        loginTo.setMac("不再获取MAC地址");
         UserLoginRo ro = svc.loginByLoginName(loginTo);
         if (LoginResultDic.SUCCESS.equals(ro.getResult())) {
             jwtSignWithCookie(ro, resp);
@@ -52,8 +64,11 @@ public class LoginCtrl {
      */
     @ApiOperation("用户通过用户名称(Email/Moblie/LoginName)登录\n(1: 成功;0: 缓存失败;-1: 参数不正确;-2: 找不到用户信息;-3: 密码错误;-4: 账号被锁定;-5: 用户用Email登录，但Email尚未通过验证;-6: 用户用手机号登录，但手机号尚未通过验证)")
     @PostMapping("/user/login/by/user/name")
-    UserLoginRo loginByUserName(@RequestBody LoginByUserNameTo loginTo, HttpServletResponse resp) {
+    UserLoginRo loginByUserName(@RequestBody LoginByUserNameTo loginTo, HttpServletRequest req, HttpServletResponse resp) {
         _log.info("login: " + loginTo);
+        loginTo.setIp(AgentUtils.getIpAddr(req, passProxy));
+        loginTo.setUserAgent(AgentUtils.getUserAgent(req));
+        loginTo.setMac("不再获取MAC地址");
         UserLoginRo ro = svc.loginByUserName(loginTo);
         if (LoginResultDic.SUCCESS.equals(ro.getResult())) {
             jwtSignWithCookie(ro, resp);
@@ -66,8 +81,11 @@ public class LoginCtrl {
      */
     @ApiOperation("用户通过QQ登录\n(1: 成功;0: 缓存失败;-1: 参数不正确;-2: 找不到用户信息;-4: 账号被锁定)")
     @PostMapping("/user/login/by/qq")
-    UserLoginRo loginByQq(@RequestBody LoginByQqTo loginTo, HttpServletResponse resp) {
+    UserLoginRo loginByQq(@RequestBody LoginByQqTo loginTo, HttpServletRequest req, HttpServletResponse resp) {
         _log.info("login: " + loginTo);
+        loginTo.setIp(AgentUtils.getIpAddr(req, passProxy));
+        loginTo.setUserAgent(AgentUtils.getUserAgent(req));
+        loginTo.setMac("不再获取MAC地址");
         UserLoginRo ro = svc.loginByQq(loginTo);
         if (LoginResultDic.SUCCESS.equals(ro.getResult())) {
             jwtSignWithCookie(ro, resp);
@@ -80,8 +98,11 @@ public class LoginCtrl {
      */
     @ApiOperation("用户通过微信登录\n(1: 成功;0: 缓存失败;-1: 参数不正确;-2: 找不到用户信息;-4: 账号被锁定)")
     @PostMapping("/user/login/by/wx")
-    UserLoginRo loginByWx(@RequestBody LoginByWxTo loginTo, HttpServletResponse resp) {
+    UserLoginRo loginByWx(@RequestBody LoginByWxTo loginTo, HttpServletRequest req, HttpServletResponse resp) {
         _log.info("login: " + loginTo);
+        loginTo.setIp(AgentUtils.getIpAddr(req, passProxy));
+        loginTo.setUserAgent(AgentUtils.getUserAgent(req));
+        loginTo.setMac("不再获取MAC地址");
         UserLoginRo ro = svc.loginByWx(loginTo);
         if (LoginResultDic.SUCCESS.equals(ro.getResult())) {
             jwtSignWithCookie(ro, resp);
--- suc-svr/src/main/java/rebue/suc/ctrl/RegCtrl.java
+++ suc-svr/src/main/java/rebue/suc/ctrl/RegCtrl.java
@@ -1,10 +1,12 @@
 package rebue.suc.ctrl;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +22,19 @@ import rebue.suc.svc.SucUserSvc;
 import rebue.suc.to.RegByLoginNameTo;
 import rebue.suc.to.RegByQqTo;
 import rebue.suc.to.RegByWxTo;
+import rebue.wheel.AgentUtils;
 import rebue.wheel.turing.JwtUtils;
 
 @Api(tags = "用户注册")
 @RestController
 public class RegCtrl {
     private final static Logger _log = LoggerFactory.getLogger(RegCtrl.class);
+
+    /**
+     * 前面经过的代理
+     */
+    @Value("${suc.passProxy:noproxy}")
+    private String              passProxy;
 
     @Resource
     private SucUserSvc          svc;
@@ -37,8 +46,11 @@ public class RegCtrl {
      */
     @ApiOperation("用户通过登录名称注册\n(1: 成功;0: 缓存失败;-1: 参数不正确;-2: 用户登录名已存在;-3: Email已存在;-4: 手机号码已存在;-5: 身份证号码已存在;-6: QQ的ID已存在;-7: 微信的ID已存在)")
     @PostMapping("/user/reg/by/login/name")
-    UserRegRo regByLoginName(@RequestBody RegByLoginNameTo regTo, HttpServletResponse resp) {
+    UserRegRo regByLoginName(@RequestBody RegByLoginNameTo regTo, HttpServletRequest req, HttpServletResponse resp) {
         _log.info("reg by login name: {}", regTo);
+        regTo.setIp(AgentUtils.getIpAddr(req, passProxy));
+        regTo.setUserAgent(AgentUtils.getUserAgent(req));
+        regTo.setMac("不再获取MAC地址");
         UserRegRo ro = svc.regByLoginName(regTo);
         if (RegResultDic.SUCCESS.equals(ro.getResult())) {
             jwtSignWithCookie(ro, resp);
@@ -51,8 +63,11 @@ public class RegCtrl {
      */
     @ApiOperation("用户通过QQ注册\n(1: 成功;0: 缓存失败;-1: 参数不正确;-2: 用户登录名已存在;-3: Email已存在;-4: 手机号码已存在;-5: 身份证号码已存在;-6: QQ的ID已存在;-7: 微信的ID已存在)")
     @PostMapping("/user/reg/by/qq")
-    UserRegRo regByQq(@RequestBody RegByQqTo regTo, HttpServletResponse resp) {
+    UserRegRo regByQq(@RequestBody RegByQqTo regTo, HttpServletRequest req, HttpServletResponse resp) {
         _log.info("reg by qq: {}", regTo);
+        regTo.setIp(AgentUtils.getIpAddr(req, passProxy));
+        regTo.setUserAgent(AgentUtils.getUserAgent(req));
+        regTo.setMac("不再获取MAC地址");
         UserRegRo ro = svc.regByQq(regTo);
         if (RegResultDic.SUCCESS.equals(ro.getResult())) {
             jwtSignWithCookie(ro, resp);
@@ -65,8 +80,11 @@ public class RegCtrl {
      */
     @ApiOperation("用户通过微信注册\n(1: 成功;0: 缓存失败;-1: 参数不正确;-2: 用户登录名已存在;-3: Email已存在;-4: 手机号码已存在;-5: 身份证号码已存在;-6: 微信的ID已存在;-7: 微信的ID已存在)")
     @PostMapping("/user/reg/by/wx")
-    UserRegRo regByWx(@RequestBody RegByWxTo regTo, HttpServletResponse resp) {
+    UserRegRo regByWx(@RequestBody RegByWxTo regTo, HttpServletRequest req, HttpServletResponse resp) {
         _log.info("reg by wx: {}", regTo);
+        regTo.setIp(AgentUtils.getIpAddr(req, passProxy));
+        regTo.setUserAgent(AgentUtils.getUserAgent(req));
+        regTo.setMac("不再获取MAC地址");
         UserRegRo ro = svc.regByWx(regTo);
         if (RegResultDic.SUCCESS.equals(ro.getResult())) {
             jwtSignWithCookie(ro, resp);
