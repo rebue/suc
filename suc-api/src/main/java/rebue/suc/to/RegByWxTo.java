92459d17a89e78eb486fec4056e5c2fbe24128f9
zbz <nnzbz@21cn.com>
2018-10-16 10:01:34 +0800

    * 优化代码

--- suc-api/src/main/java/rebue/suc/to/RegByWxTo.java
+++ suc-api/src/main/java/rebue/suc/to/RegByWxTo.java
@@ -3,91 +3,47 @@ package rebue.suc.to;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * 注册用户(通过微信)的传输对象
  */
-@ApiModel(value = "注册(微信)", description = "用户通过微信注册的参数")
+@Getter
+@Setter
+@ToString
 @JsonInclude(Include.NON_NULL)
 public class RegByWxTo extends RegBaseTo {
-	@ApiModelProperty(value = "微信的ID", required = true)
-	private String wxId;
-	@ApiModelProperty(value = "微信昵称", required = true)
-	private String wxNickname;
-	@ApiModelProperty(value = "微信头像", required = true)
-	/**
-	 * 微信openid
-	 */
-	private String wxOpenid;
-	/**
-	 * 微信头像
-	 */
-	private String wxFace;
+    /**
+     * 微信的ID
+     */
+    private String wxId;
+    /**
+     * 微信openid
+     */
+    private String wxOpenid;
+    /**
+     * 微信昵称
+     */
+    private String wxNickname;
+    /**
+     * 微信头像
+     */
+    private String wxFace;
 
-	/**
-	 * 推广人id
-	 */
-	private Long promoterId;
+    /**
+     * 推广人id
+     */
+    private Long   promoterId;
 
-	/**
-	 * 上线id
-	 */
-	private Long onlineId;
-
-	public String getWxId() {
-		return wxId;
-	}
-
-	public void setWxId(String wxId) {
-		this.wxId = wxId;
-	}
-
-	public String getWxNickname() {
-		return wxNickname;
-	}
-
-	public void setWxNickname(String wxNickname) {
-		this.wxNickname = wxNickname;
-	}
-
-	public String getWxOpenid() {
-		return wxOpenid;
-	}
-
-	public void setWxOpenid(String wxOpenid) {
-		this.wxOpenid = wxOpenid;
-	}
-
-	public String getWxFace() {
-		return wxFace;
-	}
-
-	public void setWxFace(String wxFace) {
-		this.wxFace = wxFace;
-	}
-
-	public Long getPromoterId() {
-		return promoterId;
-	}
-
-	public void setPromoterId(Long promoterId) {
-		this.promoterId = promoterId;
-	}
-
-	public Long getOnlineId() {
-		return onlineId;
-	}
-
-	public void setOnlineId(Long onlineId) {
-		this.onlineId = onlineId;
-	}
-
-	@Override
-	public String toString() {
-		return "RegByWxTo [wxId=" + wxId + ", wxNickname=" + wxNickname + ", wxOpenid=" + wxOpenid + ", wxFace="
-				+ wxFace + ", promoterId=" + promoterId + ", onlineId=" + onlineId + "]";
-	}
+    /**
+     * 上线id
+     * TODO 删除此属性
+     * 
+     * @deprecated
+     */
+    @Deprecated
+    private Long   onlineId;
 
 }
