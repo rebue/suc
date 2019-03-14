932a516abe014a1c614bb91802c587c2b69329d2
lbl <lbl@lbl-PC>
2019-03-14 17:01:21 +0800

    组织添加联系方式后修改提交

--- suc-svr/src/main/java/rebue/suc/ctrl/SucOrgCtrl.java
+++ suc-svr/src/main/java/rebue/suc/ctrl/SucOrgCtrl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DuplicateKeyException;
